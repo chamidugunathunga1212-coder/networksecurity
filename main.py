f002b882ebb57fd4f3bed1a55d108d9f36e874c4
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-01-03 23:42:09 +0530

    done the data validation part

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from netwoksecurity.components.data_ingestion import DataIngestion
 from netwoksecurity.exception.exception import NetworkSecurityException
 from netwoksecurity.logging.logger import logging
-from netwoksecurity.entity.config_entity import DataIngestionConfig,Training_PipeLine_Config
+from netwoksecurity.entity.config_entity import DataIngestionConfig,Training_PipeLine_Config,DataVAlidationConfig
+
+from netwoksecurity.components.data_validation import DataValidation
 
 import sys
 
@@ -13,5 +15,18 @@ if __name__=='__main__':
         logging.info("Initiate the data ingestion")
         data_ingestion_artifacts=data_ingestion.initiate_data_ingestion()
         print(data_ingestion_artifacts)
+        logging.info("Data ingestion is completed")
+
+        dataValidationConfig = DataVAlidationConfig(train_pipeline_congig)
+        
+        data_validation = DataValidation(data_ingestion_artifacts,dataValidationConfig)
+        logging.info("Initiate the data validation")
+        data_validation_artifacts = data_validation.initiate_data_validation()
+        print(data_validation_artifacts)
+        logging.info("Data validation is completed")
+
+
+
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
