651e6941110328177acafd4dcfdcc75f756b7bb1
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2026-01-05 16:15:31 +0530

    done the mlflow

--- netwoksecurity/components/model_trainer.py
+++ netwoksecurity/components/model_trainer.py
@@ -26,6 +26,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 
+import mlflow
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -34,6 +36,20 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+
+    def track_mlflow(self,model,classification_metric):
+        try:
+            
+            with mlflow.start_run():
+                mlflow.sklearn.log_model(model,artifact_path="model")
+                mlflow.log_metric("f1_score",classification_metric.f1_score)
+                mlflow.log_metric("precision_score",classification_metric.precision_score)
+                mlflow.log_metric("recall_score",classification_metric.recall_score)
+                mlflow.sklearn.log_model(model,artifact_path="model")
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
         
 
     def train_model(self,X_train,y_train,x_test,y_test):
@@ -87,14 +103,14 @@ class ModelTrainer:
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
-        # ## Track the experiements with mlflow
-        # self.track_mlflow(best_model,classification_train_metric)
+        ## Track the experiements with mlflow
+        self.track_mlflow(best_model,classification_train_metric)
 
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
 
-        # self.track_mlflow(best_model,classification_test_metric)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
